a39933cebbf26bf167fa24b7e1265646db4b9477
kihun.ahn <kihun.ahn@samsung.com>
2025-11-23 17:33:05 +0900

    first commit

---
+++ HciKit.Tests/UnitTest1.cs
@@ -0,0 +1,12 @@
+﻿namespace HciKit.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        Class1 c1 = new Class1();
+
+        Assert.Equal(3, c1.Add(1, 2));
+    }
+}
