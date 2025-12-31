f871a7534dd7a975379c39e0e711b3f3cdbb4955
kihun.ahn <kihun.ahn@samsung.com>
2025-12-31 23:45:59 +0900

    Update HciEvent

--- HciKit/Parser/Events/EventParser.cs
+++ HciKit/Parser/Events/EventParser.cs
@@ -32,9 +32,19 @@ internal class EventParser
             HciEventCodes.ConnectionComplete => ConnectionCompleteEvent.Parse(ref r),
             HciEventCodes.ConnectionRequest => ConnectionRequestEvent.Parse(ref r),
             HciEventCodes.DisconnectionComplete => DisconnectionCompleteEvent.Parse(ref r),
-
+            HciEventCodes.AuthenticationComplete => AuthenticationCompleteEvent.Parse(ref r),
+            HciEventCodes.RemoteNameRequestComplete => RemoteNameRequestCompleteEvent.Parse(ref r),
+            HciEventCodes.EncryptionChangeV1 => EncryptionChangeV1Event.Parse(ref r),
+            HciEventCodes.ChangeConnectionLinkKeyComplete => ChangeConnectionLinkKeyCompleteEvent.Parse(ref r),
+            HciEventCodes.LinkKeyTypeChanged => LinkKeyTypeChangedEvent.Parse(ref r),
+            HciEventCodes.ReadRemoteSupportedFeaturesComplete => ReadRemoteSupportedFeaturesCompleteEvent.Parse(ref r),
+            HciEventCodes.ReadRemoteVersionInformationComplete => ReadRemoteVersionInformationCompleteEvent.Parse(ref r),
+            HciEventCodes.QoSSetupComplete => QoSSetupCompleteEvent.Parse(ref r),
             HciEventCodes.CommandComplete => CommandCompleteEvent.Parse(ref r),
             HciEventCodes.CommandStatus => CommandStatusEvent.Parse(ref r),
+
+            HciEventCodes.EncryptionChangeV2 => EncryptionChangeV2Event.Parse(ref r),
+
             _ => new UnknownHciPacket(HciUnknownReason.UnsupportedEventCode, HciPacketType.Event, eventCode)
         };
     }
